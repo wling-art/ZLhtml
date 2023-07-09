0b976a3937f7e2c1bf0925ef57a0eca331048b08
wlingzhenyu <wlingzhenyu@163.com>
2023-07-09 23:53:49 +0800

    modified:   .venv/Lib/site-packages/Flask-2.2.3.dist-info/RECORD

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Users\HP\Desktop\code\html\.venv
+set VIRTUAL_ENV=d:\Users\HP\Desktop\code\html\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(zlhtml) %PROMPT%
+set PROMPT=(.venv) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(zlhtml) 
+set VIRTUAL_ENV_PROMPT=(.venv) 
 
 :END
 if defined _OLD_CODEPAGE (
