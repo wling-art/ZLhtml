6d5f2ec9378b18cd9c871162c837309f62052f83
wlingzhenyu <wlingzhenyu@163.com>
2023-07-10 17:09:24 +0800

    删注释

--- app.py
+++ app.py
@@ -9,7 +9,14 @@ from model.check_login import is_existed, exist_user, is_null
 from model.check_regist import add_user
 from flask_wtf.csrf import CSRFProtect
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, EmailField, BooleanField, Form
+from wtforms import (
+    StringField,
+    PasswordField,
+    SubmitField,
+    EmailField,
+    BooleanField,
+    Form,
+)
 from wtforms.validators import DataRequired
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
@@ -24,6 +31,7 @@ class LoginForm(FlaskForm):
     cheackbox = BooleanField('Cheackbox')
     submit = SubmitField('GO~')
 
+
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = EmailField('Email', validators=[DataRequired()])
@@ -37,7 +45,6 @@ class RegisterForm(FlaskForm):
 @app.route('/index')
 def index():
     return render_template('index.html')
-    # return redirect(url_for('user_login'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -45,10 +52,12 @@ def user_login():
     login_form = LoginForm()
     register_form = RegisterForm()
     if request.method == 'POST':  # 注册发送
-        print(login_form.validate_on_submit(),
-              register_form.validate_on_submit(), request.form)
-        if login_form.validate_on_submit() and 'GO~' in request.form.get(
-                'submit'):
+        print(
+            login_form.validate_on_submit(),
+            register_form.validate_on_submit(),
+            request.form,
+        )
+        if login_form.validate_on_submit() and 'GO~' in request.form.get('submit'):
             username = login_form.username.data
             password = login_form.password.data
             rbpwd = login_form.cheackbox.data
@@ -59,34 +68,43 @@ def user_login():
                 # 转到页面
                 return redirect(url_for('index'))
             elif exist_user(username):
-                return render_template('login.html',
-                                       message="密码错误!!!笨蛋!",
-                                       login_form=login_form,
-                                       register_form=register_form)
+                return render_template(
+                    'login.html',
+                    message="密码错误!!!笨蛋!",
+                    login_form=login_form,
+                    register_form=register_form,
+                )
             else:
-                return render_template('login.html',
-                                       message="骗子！你根本不存在！",
-                                       login_form=login_form,
-                                       register_form=register_form)
+                return render_template(
+                    'login.html',
+                    message="骗子！你根本不存在！",
+                    login_form=login_form,
+                    register_form=register_form,
+                )
 
-        if register_form.validate_on_submit(
-        ) and 'Join us~' in request.form.get('submit'):
+        if register_form.validate_on_submit() and 'Join us~' in request.form.get(
+            'submit'
+        ):
             username = register_form.username.data
             email = register_form.email.data
             password = register_form.password.data
             if exist_user(username):
-                return render_template('login.html',
-                                       message="拜托，有人也在用这个用户名欸!",
-                                       login_form=login_form,
-                                       register_form=register_form)
+                return render_template(
+                    'login.html',
+                    message="拜托，有人也在用这个用户名欸!",
+                    login_form=login_form,
+                    register_form=register_form,
+                )
             add_user(username, email, password)
-            return render_template('login.html',
-                                   login_form=login_form,
-                                   register_form=register_form,
-                                   message="你的注册很成功,可我想让你重新登陆！")
-    return render_template('login.html',
-                           login_form=login_form,
-                           register_form=register_form)
+            return render_template(
+                'login.html',
+                login_form=login_form,
+                register_form=register_form,
+                message="你的注册很成功,可我想让你重新登陆！",
+            )
+    return render_template(
+        'login.html', login_form=login_form, register_form=register_form
+    )
 
 
 if __name__ == "__main__":
