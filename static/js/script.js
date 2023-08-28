9729ca3257936bc4542a7197c464536e21e6d455
wling <wlingzhenyu@163.com>
2023-08-28 09:01:22 +0800

    fix Should not have unused variables

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,15 @@
-const wrapper = document.querySelector(".wrapper");
 const loginLink = document.querySelector(".login-link");
 const registerLink = document.querySelector(".register-link");
-const btnPopup = document.querySelector(".btnLogin-popup");
-const iconClose = document.querySelector(".icon-close");
 
 registerLink.addEventListener("click", () => {
-  wrapper.classList.add("active");
+    wrapper.classList.add("active");
 });
 
 loginLink.addEventListener("click", () => {
-  wrapper.classList.remove("active");
+    wrapper.classList.remove("active");
 });
-
-// btnPopup.addEventListener('click', () => {
-//     wrapper.classList.add('active-popup');
-// });
-
-// iconClose.addEventListener('click', () => {
-//     wrapper.classList.remove('active-popup');
-// });
 
 // 等待网页加载完执行wrapper.classList.add('active-popup');
 window.onload = function () {
-  wrapper.classList.add("active-popup");
+    wrapper.classList.add("active-popup");
 };
