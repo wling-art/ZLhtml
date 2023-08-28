a00ab2ddfd6c621c60eb4032bf336be37bab8f06
wling <wlingzhenyu@163.com>
2023-08-28 09:05:34 +0800

    fix 未声明变量的用法

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,16 @@
+const wrapper = document.querySelector(".wrapper");
 const loginLink = document.querySelector(".login-link");
 const registerLink = document.querySelector(".register-link");
 
 registerLink.addEventListener("click", () => {
-  wrapper.classList.add("active");
+    wrapper.classList.add("active");
 });
 
 loginLink.addEventListener("click", () => {
-  wrapper.classList.remove("active");
+    wrapper.classList.remove("active");
 });
 
 // 等待网页加载完执行wrapper.classList.add('active-popup');
 window.onload = function () {
-  wrapper.classList.add("active-popup");
+    wrapper.classList.add("active-popup");
 };
