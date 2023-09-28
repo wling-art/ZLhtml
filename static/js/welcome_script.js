f9839e345598fe3efb18c0c0ae39901dceeadc3d
wling <wlingzhenyu@163.com>
2023-09-28 21:12:19 +0800

    增加了实时倒计时

--- static/js/welcome_script.js
+++ static/js/welcome_script.js
@@ -1,4 +1,15 @@
-//五秒后自动跳转到登录页面
-setTimeout(() => {
-  location.href = "index";
-}, 5000);
+
+let time = 5;
+function showTime() {
+
+    time--;
+    if (time <= 0) {
+        //clearInterval(id);
+
+        location.href = "http://www.baidu.com";
+    }
+    let sp = document.getElementById("time");
+    sp.innerHTML = time;
+}
+
+let id = setInterval(showTime, 1000);
